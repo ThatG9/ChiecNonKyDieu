94e29a5938f3a8a850ad1d6371cbb5f7fb674dd3
Akhoa00 <hakoklo123@gmail.com>
2025-08-15 02:23:20 +0700

    Update rank.js

--- rank.js
+++ rank.js
@@ -1,19 +1,55 @@
 let leaderboardData = [];
+
+function saveLeaderboard() {
+    localStorage.setItem('leaderboardData', JSON.stringify(leaderboardData));
+}
+
+function loadLeaderboard() {
+    const savedData = localStorage.getItem('leaderboardData');
+    if (savedData) {
+        leaderboardData = JSON.parse(savedData);
+    }
+}
+
+
+function displayLeaderboard() {
+    const tableBody = document.getElementById("leaderboardBody");
+    tableBody.innerHTML = "";
+    leaderboardData.sort((a, b) => b.score - a.score);
+    const top10Players = leaderboardData.slice(0, 10);
+    top10Players.forEach((entry, index) => {
+        const row = tableBody.insertRow();
+        row.innerHTML = `
+            <td class="rank">${index + 1}</td>
+            <td class="player-name">${entry.name}</td>
+            <td class="player-score">${entry.score}</td>
+        `;
+    });
+}
+
 function saveCurrentScore() {
     if (gameData.playerName && gameData.currentScore) {
-       /* tìm người chơi trong bxh*/
+        /* tìm người chơi trong bxh*/
         const existingPlayerIndex = leaderboardData.findIndex(entry => entry.name === gameData.playerName);
 
         if (existingPlayerIndex !== -1) {
             /*nếu tìm thấy tên người chơi trong bxh thì cập nhật điểm số*/
-            leaderboardData[existingPlayerIndex].score += gameData.currentScore;}
+            leaderboardData[existingPlayerIndex].score += gameData.currentScore;
+        }
         else {
             /*nếu duyệt không thấy thì tạo mục mới*/
             const scoreEntry = {
                 name: gameData.playerName,
-                score: gameData.currentScore};
-            leaderboardData.push(scoreEntry);}
+                score: gameData.currentScore
+            };
+            leaderboardData.push(scoreEntry);
+        }
+        gameData.currentScore = 0;
+        updateScore();
         
+        saveLeaderboard();
+        displayLeaderboard();
+
         /*Thông báo*/
         alert('Điểm của bạn đã được lưu vào bảng xếp hạng!');
     } else {
@@ -21,24 +57,8 @@ function saveCurrentScore() {
     }
 }
 
-function displayLeaderboard() {
-    /* Lấy phần tử tbody của bảng xếp hạng*/
-    const tableBody = document.querySelector('#leaderboardBody');
+window.addEventListener('load', () => {
+    loadLeaderboard();
+    displayLeaderboard();
+});
 
-    /* Xóa các hàng cũ để cập nhật*/
-    tableBody.innerHTML = '';
-
-    /*Sắp xếp theo điểm số giảm dần*/
-    leaderboardData.sort((a, b) => b.score - a.score);
-
-    /* Duyệt mảng và tạo hàng */
-   leaderboardData.forEach(function(entry, index) {
-    const row = tableBody.insertRow();
-        row.innerHTML = `
-            <td class="rank">${index + 1}</td>
-            <td class="player-name">${entry.name}</td>
-            <td class="player-score">${entry.score}</td>
-        `;
-    });
-
-}
